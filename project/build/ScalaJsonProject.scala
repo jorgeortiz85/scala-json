52aaf97a5b2f54c11160143eb1a694524df368f7
Jorge Ortiz <jorge.ortiz@gmail.com>
2010-08-06 11:06:16 -0700

    This breaks stuff, getting rid of it

--- project/build/ScalaJsonProject.scala
+++ project/build/ScalaJsonProject.scala
@@ -2,23 +2,6 @@ import sbt._
 import com.twitter.sbt._
 
 
-class ScalaJsonProject(info: ProjectInfo) extends StandardProject(info) {
-  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
-
-  Credentials(Path.userHome / ".ivy2" / "credentials", log)
-  val publishTo = "nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
-
-  override def pomExtra =
-    <licenses>
-      <license>
-        <name>Apache 2</name>
-        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-        <distribution>repo</distribution>
-      </license>
-      <license>
-        <name>Scala License</name>
-        <url>http://www.scala-lang.org/node/146</url>
-        <distribution>repo</distribution>
-      </license>
-    </licenses>
+class ScalaJsonProject(info: ProjectInfo) extends DefaultProject(info) {
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
 }
